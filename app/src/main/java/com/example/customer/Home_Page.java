49a6f26f3e4ffab71b800eefce99f3b6d3d075b7
BhattKhushant <khushant306@gmail.com>
2022-08-05 22:57:01 +0530

    Data Retrive

--- app/src/main/java/com/example/customer/Home_Page.java
+++ app/src/main/java/com/example/customer/Home_Page.java
@@ -1,14 +1,71 @@
 package com.example.customer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Home_Page extends AppCompatActivity {
+
+    ArrayAdapter<String>  AD_TN;
+    ArrayList<String>  AL_TN;
+
+    Spinner sp_tn;
+
+    SharedPreferences.Editor editor;
+    SharedPreferences shrd;
+
+    DatabaseReference RE1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+        initialize();
+        RE1.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds:snapshot.getChildren()){
+                    for(DataSnapshot ds1:ds.getChildren()){
+                        for(DataSnapshot ds2:ds1.getChildren()) {
+                            AL_TN.add(ds2.getValue(Ticket.class).getTicket_No());
+                            sp_tn.setAdapter(AD_TN);
+                        }
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void initialize() {
+
+        AL_TN = new ArrayList<>();
+        AD_TN = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, AL_TN);
+
+
+        shrd = getSharedPreferences("Customer", MODE_PRIVATE);
+        editor = shrd.edit();
+
+        RE1 = FirebaseDatabase.getInstance().getReference("Customer").child(shrd.getString("CID",null));
     }
 }
--- app/src/main/java/com/example/customer/Login_Activity.java
+++ app/src/main/java/com/example/customer/Login_Activity.java
@@ -1,6 +1,7 @@
 package com.example.customer;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,18 +22,30 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Login_Activity extends AppCompatActivity {
 
+    SharedPreferences.Editor editor;
+    SharedPreferences shrd;
+
     String Email, PASSWORD, Con_Pass, USER, CARDID, BAL, ADD;
 
-    boolean cond = false,PW=false;
+    boolean cond = false, PW = false;
 
-    TextInputEditText mail, pass, c_pass,s_pw;
+    TextInputEditText mail, pass, c_pass, s_pw;
 
-    FrameLayout fm1, fm2,fm3;
+    FrameLayout fm1, fm2, fm3;
     Button btn_LG_vf;
 
     User user;
 
     DatabaseReference RE1;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (shrd.contains("CID")) {
+            startActivity(new Intent(Login_Activity.this, Home_Page.class));
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,8 +69,8 @@ public class Login_Activity extends AppCompatActivity {
                                     BAL = ds.getValue(User.class).getBalance();
                                     ADD = ds.getValue(User.class).getAddress();
                                     CARDID = ds.getValue(User.class).getCardID();
-                                    if(ds.getValue(User.class).getPassword()!=null){
-                                        PW=true;
+                                    if (ds.getValue(User.class).getPassword() != null) {
+                                        PW = true;
                                     }
                                     break;
                                 } else {
@@ -65,11 +78,11 @@ public class Login_Activity extends AppCompatActivity {
                                 }
                             }
                             if (cond) {
-                                if(!PW){
+                                if (!PW) {
                                     fm1.setVisibility(View.GONE);
                                     fm2.setVisibility(View.VISIBLE);
                                     btn_LG_vf.setText("Sign UP");
-                                }else{
+                                } else {
                                     fm1.setVisibility(View.GONE);
                                     fm3.setVisibility(View.VISIBLE);
                                     btn_LG_vf.setText("Sign IN");
@@ -84,44 +97,49 @@ public class Login_Activity extends AppCompatActivity {
 
                         }
                     });
-                }
-                else if(fm2.getVisibility()==View.VISIBLE) {
+                } else if (fm2.getVisibility() == View.VISIBLE) {
                     PASSWORD = pass.getText().toString();
                     Con_Pass = c_pass.getText().toString();
-                    if (!PASSWORD.equals(CARDID)){
+                    if (!PASSWORD.equals(CARDID)) {
                         if (PASSWORD.equals(Con_Pass)) {
                             DatabaseReference RE2 = FirebaseDatabase.getInstance().getReference("Customer").child(CARDID);
                             user.setPassword(PASSWORD);
                             RE2.child("password").setValue(user.getPassword()).addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
+                                    if (task.isSuccessful()) {
                                         fm2.setVisibility(View.GONE);
                                         fm3.setVisibility(View.VISIBLE);
                                     }
                                 }
                             });
                         }
-                    }else{
+                    } else {
                         Toast.makeText(Login_Activity.this, "card id and Password can't be same", Toast.LENGTH_SHORT).show();
                     }
-                }
-                else{
+                } else {
                     DatabaseReference RE3 = RE1.getRef();
                     RE3.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            for(DataSnapshot ds: snapshot.getChildren()){
+                            for (DataSnapshot ds : snapshot.getChildren()) {
                                 if (ds.getValue(User.class).getPassword().equalsIgnoreCase(s_pw.getText().toString())) {
                                     cond = true;
                                     break;
-                                }else{
-                                    cond=false;
+                                } else {
+                                    cond = false;
                                     Toast.makeText(Login_Activity.this, "Wrong Password", Toast.LENGTH_SHORT).show();
                                 }
                             }
-                            startActivity(new Intent(Login_Activity.this,Home_Page.class));
-                            finish();
+                            if (cond) {
+                                editor.clear();
+                                editor.apply();
+                                editor.putString("CID", CARDID);
+                                editor.apply();
+
+                                startActivity(new Intent(Login_Activity.this, Home_Page.class));
+                                finish();
+                            }
                         }
 
                         @Override
@@ -136,6 +154,9 @@ public class Login_Activity extends AppCompatActivity {
     }
 
     private void initialize() {
+
+        shrd = getSharedPreferences("Customer", MODE_PRIVATE);
+        editor = shrd.edit();
 
         mail = findViewById(R.id.L_email1);
         pass = findViewById(R.id.S_L_password1);
